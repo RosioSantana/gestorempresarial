1dd511d0f4dc39f2b3bd42da8177215b16d2b845
RosioSantana <rosio.santana2242@uppuebla.edu.mx>
2024-03-25 00:07:19 -0400

    1er esqueleto

--- src/gestor/errores/GestionErrores.java
+++ src/gestor/errores/GestionErrores.java
@@ -1,20 +1,17 @@
 package gestor.errores;
+import java.util.*;
 //Práctica 11. Gestor Empresarial (Gestión de Errores)
-//Muchas dudas
 public final class GestionErrores {
-    private Map error;//DUDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    private Map<Integer,String> error;
     private boolean existeError;
     private int noError;
     private String descripcionTecnica;
-
     public GestionErrores(){
-
+        error = new HashMap<Integer,String>();
     }
-    private void cargarErrores(){
-
+    private void CargarErrores(){
     }
     public void setNoError(int a, String b){
-
     }
     public String getError(){
         return ":D";
@@ -22,7 +19,7 @@ public final class GestionErrores {
     public String getErrorTecnico(){
         return null;
     }
-    public boolean existeError(){
+    public boolean ExisteError(){
         return false;
     }
 }
