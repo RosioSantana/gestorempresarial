1592113b9a2b6666f1e1efe045a739b82696439b
LuisAgustinTorresHernandez <luis.torres2277@uppuebla.edu.mx>
2024-04-11 19:07:56 -0500

    4ta seccion DatosEmpresa

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,14 @@
 package gestor.archivos;
 import gestor.empresarial.datos.DatosEmpresariales;
+import gestor.empresarial.datos.DatosPersonales;
+import java.util.*;
 
-import java.util.Scanner;
+
+
 
 public class Principal {
     public static void main (String[] args){
-       /* ArchivoTexto file1 = new ArchivoTexto("C:\\Users\\Luis Agustin TH\\Videos\\java\\rubireprobada.txt");
+       ArchivoTexto file1 = new ArchivoTexto("C:\\Users\\Luis Agustin TH\\Videos\\java\\rubireprobada.txt");
         String linea;
         Scanner teclado = new Scanner(System.in);
 
@@ -25,11 +28,23 @@ public class Principal {
             System.out.println(linea);
             linea=file1.Leer();
         }
-        file1.Cerrar();*/
+        file1.Cerrar();
 
-        //DATOS EMPRESARIALES
-        DatosEmpresariales datos = new DatosEmpresariales(12,"gerente","2229237726","encargado de produccion");
+        //DATOS EMPRESARIALES //personales
+        Scanner te = new Scanner(System.in);
+        String adscripcion;
+        String telefonoExterior;
+        String puesto;
 
+        System.out.print("Proporciona la adscripcion: ");
+        adscripcion = te.nextLine();
+
+        System.out.print("Proporciona el telefono exterior: ");
+        telefonoExterior = te.nextLine();
+
+        System.out.print("Ingrese el puesto: ");
+        puesto = te.nextLine();
+        DatosEmpresariales datos = new DatosEmpresariales(12,adscripcion, telefonoExterior,puesto);
 
     }
 }
